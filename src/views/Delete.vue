c80b8b3daf006bb5077031e873a9e349cf61bda3
TaufiqWolf <55835348+Twolf4215@users.noreply.github.com>
2022-09-11 14:39:14 +0700

    initial commit

---
+++ src/views/Delete.vue
@@ -0,0 +1,24 @@
+<template>
+    <div >
+        <p class="text-light p-3 bg-danger bg-opacity-10 border border-info border-start-0 rounded-end">Apakah Anda yakin ingin menghapus ini ?</p>
+        <button class="btn btn-danger" @click="deleteUser()">Delete</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    methods: {
+        async deleteUser() {
+            let x = window.confirm("You want to delete the user?");
+            if (x) {
+                const user = await axios.delete(
+                    "https://reqres.in/api/users/2"
+                );
+                console.log(user);
+                alert("User deleted!");
+            }
+        },
+    },
+}
+</script>
