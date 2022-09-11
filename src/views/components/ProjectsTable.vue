47e0787c334bba82f2e0aeea0568cef88d07aca0
TaufiqWolf <55835348+Twolf4215@users.noreply.github.com>
2022-09-11 11:25:44 +0700

    initial commit

--- src/views/components/ProjectsTable.vue
+++ src/views/components/ProjectsTable.vue
@@ -1,11 +1,11 @@
-<template>
+<!-- <template>
   <div class="card mb-4">
 <div class="card-header pb-0">
 <h6>Projects table</h6>
 </div>
 <div class="card-body px-0 pt-0 pb-2">
 <div class="table-responsive p-0">
- <table class="table align-items-center justify-content-center mb-0">
+  <table class="table align-items-center justify-content-center mb-0">
 <thead>
 <tr>
 <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Project</th>
@@ -225,4 +225,4 @@
 export default {
     name: "projects-table"
 }
-</script>
+</script> -->
